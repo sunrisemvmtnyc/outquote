2c29d6eb0703a9353c0b7aa30502893e291fdb20
Alex Graves <alexwoodgraves@gmail.com>
2020-07-25 12:55:38 -0400

    improve code style and readability

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 let quote = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 let name = "First Last";
-let title = "Position";
+let title = "Title";
 let showAttribution = true;
 let showTitle = true;
 let includeLogo = true;
 let centerElements = false;
 let useWordmark = false;
-let includePhoto = false;
-let photoURL = "";
 
 const PRIMARY = "#FFDE16";
 const BACKGROUNDS = ["#33342E", "gradient"];
@@ -24,6 +22,7 @@ const SIZES = [
 ];
 const BORDERS = [25, 25, 10];
 const MARGINS = [100, 100, 50];
+const MAX_CONTAINER_WIDTH = 960;
 
 /*
  * Correctly wraps the quote text by adding each word, checking if it fits
@@ -33,8 +32,8 @@ const MARGINS = [100, 100, 50];
  */
 const wrapText = (context, text, x, maxWidth, lineHeight, canvasHeight) => {
   const grafs = text.split("\n");
-  let y = 0;
   const textToFill = [];
+  let y = 0;
   for (let graf of grafs) {
     const words = graf.split(" ");
     let line = "";
@@ -56,32 +55,28 @@ const wrapText = (context, text, x, maxWidth, lineHeight, canvasHeight) => {
 
   // subtract the last lineHeight and calculate adjusted height to center text
   y -= lineHeight;
-  const adjustedHeight = canvasHeight / 2 - y / 2;
+  const adjustedHeight = (canvasHeight - y) / 2;
 
   // fill text with adjusted y values
   textToFill.forEach(item => {
     [line, x, y] = [...item];
     context.fillText(line, x, y + adjustedHeight);
   });
-
-  return y;
 }
 
 /*
  * This the main function; it goes through all of the various options and
  * renders based on how the user has selected them.
  */
-const renderContent = () => {
+const render = () => {
   // get the container's width to use in sizing calculations
-  const container = document.getElementById("container");
-  let containerWidth = container.offsetWidth;
+  let containerWidth = document.getElementById("container").offsetWidth;
   if (containerWidth < 400) {
     // subtract 40 for the container's padding (20px on left and right)
     containerWidth -= 40;
   }
   // the container's max width is 960, so get whichever is smaller
-  const MAX_WIDTH = 960;
-  containerWidth = Math.min(containerWidth, MAX_WIDTH);
+  containerWidth = Math.min(containerWidth, MAX_CONTAINER_WIDTH);
 
   // resize the canvas based on the selected social media platform
   const canvas = document.getElementById("canvas");
@@ -93,8 +88,7 @@ const renderContent = () => {
   let aspect = containerWidth / canvas.width;
   // on desktop, reduce the size of insta posts/stories so they're not so tall
   if (containerWidth > 700 && containerWidth < canvas.height) {
-    aspect /= 2;
-    aspect *= (canvas.width / canvas.height);
+    aspect *= 0.5 * canvas.width / canvas.height;
   }
   canvas.style.transform = `scale(${aspect})`;
 
@@ -113,15 +107,16 @@ const renderContent = () => {
   quoteCtx.fillRect(0, 0, canvas.width, canvas.height);
 
   // then fill the background with the selection
-  const gradient = quoteCtx.createLinearGradient(
-    canvas.width / 2, 0, canvas.width / 2, canvas.height);
+  const half = canvas.width / 2;
+  const gradient = quoteCtx.createLinearGradient(half, 0, half, canvas.height);
   if (hasGradient) {
     gradient.addColorStop(0, GRADIENT[0]);
     gradient.addColorStop(0.5, GRADIENT[1]);
     gradient.addColorStop(1, GRADIENT[2]);
   }
   quoteCtx.fillStyle = hasGradient ? gradient : BACKGROUNDS[scheme];
-  quoteCtx.fillRect(BORDERS[size],
+  quoteCtx.fillRect(
+    BORDERS[size],
     BORDERS[size],
     canvas.width - BORDERS[size] * 2,
     canvas.height - BORDERS[size] * 2
@@ -138,7 +133,7 @@ const renderContent = () => {
   wrapText(
     quoteCtx,
     "\“" + quote + "\”",
-    centerElements ? canvas.width / 2 : MARGINS[size],
+    centerElements ? half : MARGINS[size],
     canvas.width - MARGINS[size] * 2,
     FONT_SIZE[size] + 10,
     canvas.height
@@ -172,8 +167,8 @@ const renderContent = () => {
     const titleLength = titleCtx.measureText(title).width;
 
     const centerPos = showTitle ?
-      canvas.width / 2 - nameLength / 2 - titleLength / 2 :
-      canvas.width / 2 - nameLength / 2;
+      half - nameLength / 2 - titleLength / 2 :
+      half - nameLength / 2;
     const nameCtxX = centerElements ? centerPos : MARGINS[size];
     const titleCtxX = nameLength + nameCtxX;
     const yPos = canvas.height - MARGINS[size] - BORDERS[size];
@@ -187,12 +182,10 @@ const renderContent = () => {
       titleCtx.font = `400 ${FONT_SIZE[size]}px source sans pro`;
       titleCtx.fillText(title, titleCtxX, yPos);
     }
-	}
+  }
 }
 
-window.setTimeout(function() {
-  renderContent();
-}, 700);
+window.setTimeout(render, 700);
 
 document.getElementById("quoteBox").oninput = function() {
   quote = this.value;
@@ -203,77 +196,68 @@ document.getElementById("quoteBox").oninput = function() {
   quote = quote.replace(/'(?=\b|$)/g, "\‘");
   quote = quote.replace(/\b"/g, "\”");
   quote = quote.replace(/"(?=\w|$)/g, "\“");
-  renderContent();
+  render();
 }
 
 document.getElementById("quoteAttr").oninput = function() {
   name = this.value;
-  renderContent();
+  render();
 }
 
 document.getElementById("quoteTitle").oninput = function() {
   title = this.value;
-  renderContent();
+  render();
 }
 
 document.getElementById("saveButton").addEventListener("click", function() {
   const dataURL = canvas.toDataURL("image/png");
-  const data = atob(dataURL.substring("data:image/png;base64,".length)),
-    asArray=new Uint8Array(data.length);
-  for (let i = 0, len = data.length; i < len; ++i) {
+  const data = atob(dataURL.substring("data:image/png;base64,".length));
+  const asArray = new Uint8Array(data.length);
+  for (let i = 0; i < data.length; ++i) {
     asArray[i] = data.charCodeAt(i);
   }
-  const blob = new Blob([asArray.buffer], {
-    type: "image/png"
-  });
+  const blob = new Blob([asArray.buffer], {type: "image/png"});
   saveAs(blob, "quote.png");
 });
 
 // EVENT HANDLERS
 
 // Resize window
-window.addEventListener("resize", renderContent);
+window.addEventListener("resize", render);
 
 // Change selected canvas size
-const canvasSize = document.getElementById("canvasSize");
-canvasSize.addEventListener("change", renderContent);
+document.getElementById("canvasSize").addEventListener("change", render);
 
 // Toggle attribution
-const toggleAttrCheckbox = document.getElementById("toggleAttribution");
-toggleAttrCheckbox.addEventListener("click", function() {
-	showAttribution = !showAttribution;
-	renderContent();
+document.getElementById("toggleAttribution").addEventListener("click", () => {
+  showAttribution = !showAttribution;
+  render();
 });
 
 // Toggle attribution title
-const toggleAttrTitleCheckbox = document.getElementById("toggleAttributionTitle");
-toggleAttrTitleCheckbox.addEventListener("click", function() {
-	showTitle = !showTitle;
+document.getElementById("toggleTitle").addEventListener("click", () => {
+  showTitle = !showTitle;
   name = document.getElementById("quoteAttr").value || "First Last";
-	renderContent();
+  render();
 });
 
 // Toggle center elements
-const toggleCenterCheckbox = document.getElementById("centerElements");
-toggleCenterCheckbox.addEventListener("click", function() {
-	centerElements = !centerElements;
-	renderContent();
+document.getElementById("centerElements").addEventListener("click", () => {
+  centerElements = !centerElements;
+  render();
 });
 
 // Change selected background/color scheme
-const backgroundColor = document.getElementById("backgroundColor");
-backgroundColor.addEventListener("change", renderContent);
+document.getElementById("backgroundColor").addEventListener("change", render);
 
 // Toggle wordmark
-const useWordmarkCheckbox = document.getElementById("useWordmark");
-useWordmarkCheckbox.addEventListener("click", function() {
+document.getElementById("useWordmark").addEventListener("click", () => {
   useWordmark = !useWordmark;
-	renderContent();
+  render();
 });
 
 // Include logo
-const toggleLogoCheckbox = document.getElementById("toggleLogo");
-toggleLogoCheckbox.addEventListener("click", function() {
+document.getElementById("toggleLogo").addEventListener("click", () => {
   includeLogo = !includeLogo;
-  renderContent();
+  render();
 });
